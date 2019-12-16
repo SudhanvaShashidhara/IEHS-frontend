f2ad5a3d4554734c7974357a862ad353b742eb6b
SudhanvaShashidhara <sudhanvashashidhara@gmail.com>
2019-12-16 19:56:41 +0530

    Added images on home page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,34 +1,161 @@
 <style>
-  h1 {
-    text-align: center;
-    margin: 0 auto;
+  section.hero-image {
+    position: relative;
+  }
+  img {
+    width: 100%;
   }
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
+  section.hero-image::before {
+    content: "";
+    position: absolute;
+    background-color: var(--main-secondary-variant);
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .content-box {
+    position: absolute;
+    text-align: center;
+    color: #fff;
+    background-color: var(--main-color);
+    width: 60%;
+    margin: auto;
+    bottom: 0;
+    left: 20%;
+    padding: 0.5rem;
+  }
+
+  .o-wrapper {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  .o-card {
+    padding: 1rem;
+  }
+
+  .o-image {
+    position: relative;
+  }
+
+  .o-image::after {
+    content: "";
+    position: absolute;
+    border-right: 3rem solid var(--main-secondary-color);
+    border-top: 3rem solid transparent;
+    right: 0;
+    bottom: 0.35rem;
+  }
+
+  .o-info button {
+    padding: 0.5rem;
+    border: 0.1rem solid var(--main-color-variant);
+    background-color: transparent;
+    color: var(--main-color-variant);
+    transition: all 0.3s ease-in;
+  }
+
+  .o-info button:hover,
+  .o-info button:active {
+    background-color: var(--main-color-variant);
+    color: var(--main-secondary-color);
+    border-color: var(--main-secondary-color);
   }
 
   @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+    section.hero-image img {
+      max-height: 80vh;
+    }
+
+    .content-box {
+      background-color: var(--main-color-variant);
+      top: 1.5rem;
+      bottom: auto;
+      width: 25%;
+      left: 37.5%;
+    }
+
+    .o-container {
+      width: 80%;
+      margin: 2rem auto;
+    }
+
+    .o-wrapper {
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+
+    .o-card {
+      min-width: 30rem;
+      max-width: 35rem;
     }
   }
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Ideal English High School – Bengaluru, Karnataka</title>
 </svelte:head>
 
-<h1>Home Page</h1>
-<div class="carousel">
-  <button class="carousel__button" />
-  <div class="carousel__track-container">
-    <ul class="carousel__track">
-      li
-    </ul>
-  </div>
-  <button class="carousel__button" />
-</div>
+<main>
+  <section class="hero-image">
+    <img src="./Home-hero.jpg" alt="Home Page" />
+    <div class="content-box">
+      <h3>Preparing & Nurturing</h3>
+      <p>Global Citizens of the future</p>
+    </div>
+  </section>
+
+  <section class="o-container">
+    <div class="o-wrapper">
+
+      <div class="o-card">
+        <div class="o-image">
+          <img
+            src="./home-connection-section-1.jpg"
+            alt="home-connection-section-1" />
+        </div>
+        <div class="o-info">
+          <h3>Academics @ IEHS</h3>
+          <p>
+            Providing each student with the opportunity to achieve, through an
+            exciting and progressive curriculum.
+          </p>
+          <button>Read More</button>
+
+        </div>
+      </div>
+
+      <div class="o-card">
+        <div class="o-image">
+          <img
+            src="./home-connection-section-2.jpg"
+            alt="home-connection-section-2" />
+        </div>
+        <div class="o-info">
+          <h3>Admission Details</h3>
+          <p>Admissions are open for the academic year 2018-19.</p>
+          <button>Read More</button>
+        </div>
+      </div>
+
+      <div class="o-card">
+        <div class="o-image">
+          <img
+            src="./home-connection-section-3.jpg"
+            alt="home-connection-section-3" />
+        </div>
+        <div class="o-info">
+          <h3>Events @ IEHS</h3>
+          <p>Take a look at latest happenings.</p>
+          <button>Read More</button>
+        </div>
+      </div>
+
+    </div>
+  </section>
+</main>
--- src/routes/mission.svelte
+++ src/routes/mission.svelte
@@ -1,19 +1,105 @@
 <style>
-  h1 {
-    text-align: center;
-    margin: 0 auto;
+  #hero-image {
+    width: 100%;
+    position: relative;
   }
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
+  #hero-image img {
+    width: 100%;
+  }
+
+  #hero-image::before {
+    content: "";
+    background-color: var(--main-secondary-variant);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .hero-image__title {
+    position: absolute;
+    top: 45%;
+    text-align: center;
+    width: 100%;
+    font-weight: 900;
+    color: white;
+  }
+
+  #mission-statement {
+    color: #fff;
+    background-color: var(--main-color);
+    padding: 1.2rem;
+  }
+
+  #mission-statement > div {
+    text-align: center;
+  }
+
+  #mission-statement > div > h4 {
+    padding: 1.2rem auto;
+  }
+
+  #mission-statement > div > div {
+    margin-top: 1rem;
+  }
+
+  #mission-statement p {
+    margin-bottom: 1rem;
+  }
+
+  #content {
+    margin: 1rem 1rem;
+  }
+
+  #title {
+    margin: 0 0 1.2rem 0;
+  }
+
+  ul {
+    margin: 0.9rem 0.5rem 0;
+  }
+
+  li {
+    position: relative;
+  }
+
+  li::before {
+    content: "*";
+    position: absolute;
+    left: -0.6rem;
+    top: 2px;
   }
 
   @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+    #mission-statement > div {
+      width: 70%;
+      margin: auto;
+    }
+    #hero-image {
+      height: 35vh;
+      width: 100%;
+    }
+
+    #hero-image img {
+      height: 100%;
+    }
+
+    .hero-image__title {
+      font-size: 2rem;
+    }
+
+    #content {
+      width: 70%;
+      margin: 2rem auto;
+    }
+
+    li::before {
+      content: "*";
+      position: absolute;
+      left: -1.2rem;
+      top: 2px;
     }
   }
 </style>
@@ -22,4 +108,84 @@
   <title>Mission Page</title>
 </svelte:head>
 
-<h1>Mission Page</h1>
+<section id="hero-image">
+  <img src="./mission-01.jpg" alt="School Attendance" />
+  <div class="hero-image__title">MISSION</div>
+</section>
+
+<section id="mission-statement">
+  <div class="content">
+    <h4>MISSION STATEMENT</h4>
+    <div>
+      <p>
+        To equip and prepare our students to withstand the winds of change
+        during their formative years and to acquire knowledge and skills which
+        will enable them to attain mastery in their chosen sphere when they step
+        out into a wider arena.
+      </p>
+      <p>
+        To fortify values which strengthen character that will stand true even
+        in the most trying situations in the laboratory of practical adult life.
+      </p>
+      <p>
+        To inculcate basic personal and professional values and produce
+        successful leaders for our swiftly changing planet, in tune with
+        international demands and requirements.
+      </p>
+    </div>
+  </div>
+</section>
+
+<section id="content">
+  <h4 id="title">MISSION AND OBJECTIVES</h4>
+  <div>
+    <strong>
+      The objective of our institution is to create an effective schooling
+      system by :
+      <ul>
+        <li>
+          Exposing children to a variety of situations and experiences, which
+          will prepare them to interact positively.
+        </li>
+        <li>
+          Developing in students an attitude to self-learning and selfdirected
+          learning for entering general as well as vocational streams of
+          education.
+        </li>
+        <li>
+          Providing the students with the latest hi-tech facilities that
+          facilitate optimum learning with minimum effort with the help of
+          electronic and satellite communication media.
+        </li>
+        <li>
+          Including high level skills and competence required for achieving
+          vocational development and entrepreneurship skills.
+        </li>
+        <li>
+          Creating an awareness among students with the help of the curriculum
+          about their role in the society and to making each of our students a
+          complete individual with a well-balanced personality.
+        </li>
+        <li>
+          Giving opportunities to all students, irrespective of their social and
+          intellectual standing to excel in all fields of development.
+        </li>
+        <li>
+          Fostering in students a sense of pride in our culture and heritage.
+        </li>
+        <li>
+          Building and effective and highly engaged community among students,
+          staff and the parent community.
+        </li>
+        <li>
+          Encouraging an inclusive environment and inculcating teamwork and
+          healthy competition among our students.
+        </li>
+        <li>
+          Providing students & parents with an exclusive nature cum health club
+          facility with all modern amenities.
+        </li>
+      </ul>
+    </strong>
+  </div>
+</section>
